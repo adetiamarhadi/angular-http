348eb3868f6d77bcec5d3de38536d3dab7dc8613
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-01-06 15:37:13 +0700

    showing a loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-49595-default-rtdb.firebaseio.com/posts.json').pipe(
       map(responseData => {
         const postsArray: Post[] = [];
@@ -49,6 +51,7 @@ export class AppComponent implements OnInit {
       })
     ).subscribe(
       posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       }
     );
